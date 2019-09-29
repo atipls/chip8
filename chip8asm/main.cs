062248f5bc349173d74ce552a251757a53d51189
atipls <ati123a@outlook.com>
2019-09-29 14:43:59 +0200

    add chip8asm and rename the solution

---
+++ chip8asm/main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chip8asm {
+    public partial class main : Form {
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new main());
+        }
+        public main() {
+            InitializeComponent();
+        }
+    }
+}
