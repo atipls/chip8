27e055e69079b6ed2cc468ddf267df2900285ce3
atipls <ati123a@outlook.com>
2019-10-16 21:52:14 +0200

    disasm cleanup

--- chip8emu/disasm.cs
+++ chip8emu/disasm.cs
@@ -13,6 +13,11 @@ namespace chip8emu {
         readonly chip8 chip;
         static class printer {
             public static string run(ushort instr) {
+                var x = (instr & 0x0F00) >> 8;
+                var y = (instr & 0x00F0) >> 4;
+                var nn = instr & 0x00FF;
+                var nnn = instr & 0x0FFF;
+
                 switch ((instr & 0xF000) >> 12) {
                     case 0:
                         switch (instr & 0x000F) {
@@ -20,48 +25,48 @@ namespace chip8emu {
                             case 0x000E: return "RET"; //return from subroutine
                         }
                         goto default;
-                    case 1: return $"JMP ${instr & 0x0FFF:X4}"; //jump
-                    case 2: return $"JSR ${instr & 0x0FFF:X4}"; //call
-                    case 3: return $"SEQ V{(instr & 0x0F00) >> 8}, ${instr & 0x00FF:X4}"; ; //skip next instruction if equal to byte
-                    case 4: return $"SNE V{(instr & 0x0F00) >> 8}, ${instr & 0x00FF:X4}"; ; //skip next instruction if not equal to byte
-                    case 5: return $"SEQ V{(instr & 0x0F00) >> 8}, V{(instr & 0x00F0) >> 4}"; //skip next instruction if equal to register
-                    case 6: return $"SET V{(instr & 0x0F00) >> 8}, ${instr & 0x00FF:X4}"; //set register to value
-                    case 7: return $"ADD V{(instr & 0x0F00) >> 8}, ${instr & 0x00FF:X4}"; //add value to register
+                    case 1: return $"JMP ${nnn:X4}"; //jump
+                    case 2: return $"JSR ${nnn:X4}"; //call
+                    case 3: return $"SEQ V{x}, ${nn:X4}"; ; //skip next instruction if equal to byte
+                    case 4: return $"SNE V{x}, ${nn:X4}"; ; //skip next instruction if not equal to byte
+                    case 5: return $"SEQ V{x}, V{y}"; //skip next instruction if equal to register
+                    case 6: return $"SET V{x}, ${nn:X4}"; //set register to value
+                    case 7: return $"ADD V{x}, ${nn:X4}"; //add value to register
                     case 8:
                         switch (instr & 0x000F) {
-                            case 0: return $"SET V{(instr & 0x0F00) >> 8}, V{(instr & 0x00F0) >> 4}"; //set vx to vy
-                            case 1: return $"OR  V{(instr & 0x0F00) >> 8}, V{(instr & 0x00F0) >> 4}"; //or vx with vy
-                            case 2: return $"AND V{(instr & 0x0F00) >> 8}, V{(instr & 0x00F0) >> 4}"; //and vx with vy
-                            case 3: return $"XOR V{(instr & 0x0F00) >> 8}, V{(instr & 0x00F0) >> 4}"; //xor vx with vy
-                            case 4: return $"ADD V{(instr & 0x0F00) >> 8}, V{(instr & 0x00F0) >> 4}"; //add vx to vy
-                            case 5: return $"SUB V{(instr & 0x0F00) >> 8}, V{(instr & 0x00F0) >> 4}"; //subtract vx from vy
-                            case 6: return $"SHR V{(instr & 0x0F00) >> 8}"; //shift and set vx to lsb
-                            case 7: return $"SNB V{(instr & 0x00F0) >> 4}, V{(instr & 0x0F00) >> 8}"; //set vx to vy-vx
-                            case 0xE: return $"SHL V{(instr & 0x0F00) >> 8}"; //shift and set vx to msb
+                            case 0: return $"SET V{x}, V{y}"; //set vx to vy
+                            case 1: return $"OR  V{x}, V{y}"; //or vx with vy
+                            case 2: return $"AND V{x}, V{y}"; //and vx with vy
+                            case 3: return $"XOR V{x}, V{y}"; //xor vx with vy
+                            case 4: return $"ADD V{x}, V{y}"; //add vx to vy
+                            case 5: return $"SUB V{x}, V{y}"; //subtract vx from vy
+                            case 6: return $"SHR V{x}"; //shift and set vx to lsb
+                            case 7: return $"SNB V{y}, V{x}"; //set vx to vy-vx
+                            case 0xE: return $"SHL V{x}"; //shift and set vx to msb
                         }
                         goto default;
-                    case 9: return $"JNE V{(instr & 0x0F00) >> 8}, V{(instr & 0x00F0) >> 4}";  //skip next instruction if not equal to register
-                    case 0xA: return $"SET IR, ${instr & 0x0FFF:X4}"; //set ir to value
-                    case 0xB: return $"JRE ${instr & 0x0FFF:X4}"; //jump to v0 + value
-                    case 0xC: return $"RND V{(instr & 0x0F00) >> 8}, ${instr & 0x00FF:X4}"; //set register to RAND&NN
-                    case 0xD: return $"DRW V{(instr & 0x0F00) >> 8}, V{(instr & 0x00F0) >> 4}, {instr & 0x000F}"; //draw at vx, vy, height n
+                    case 9: return $"JNE V{x}, V{y}";  //skip next instruction if not equal to register
+                    case 0xA: return $"SET IR, ${nnn:X4}"; //set ir to value
+                    case 0xB: return $"JRE ${nnn:X4}"; //jump to v0 + value
+                    case 0xC: return $"RND V{x}, ${nn:X4}"; //set register to RAND&NN
+                    case 0xD: return $"DRW V{x}, V{y}, {instr & 0x000F}"; //draw at vx, vy, height n
                     case 0xE:
                         switch (instr & 0x000F) {
-                            case 0x0001: return $"SKN V{(instr & 0x0F00) >> 8}"; //skip next instruction if key vx is not pressed
-                            case 0x000E: return $"SKK V{(instr & 0x0F00) >> 8}"; //skip next instruction if key vx is pressed
+                            case 0x0001: return $"SKN V{x}"; //skip next instruction if key vx is not pressed
+                            case 0x000E: return $"SKK V{x}"; //skip next instruction if key vx is pressed
                         }
                         goto default;
                     case 0xF:
-                        switch (instr & 0x00FF) {
-                            case 7: return $"LDT V{(instr & 0x0F00) >> 8}"; //set vx to delay timer
-                            case 0xA: return $"WKY V{(instr & 0x0F00) >> 8}"; //wait for key and place it to vx (blocking)
-                            case 0x15: return $"SDT V{(instr & 0x0F00) >> 8}"; //set delay timer to vx
-                            case 0x18: return $"SST V{(instr & 0x0F00) >> 8}"; //set sound timer to vx
-                            case 0x1E: return $"ADD IR, V{(instr & 0x0F00) >> 8}"; //add vx to ir
-                            case 0x29: return $"LSP V{(instr & 0x0F00) >> 8}"; //load sprite to i from vx
-                            case 0x33: return $"BCD V{(instr & 0x0F00) >> 8}"; //store bcd of vx in i
-                            case 0x55: return $"STO V0-V{(instr & 0x0F00) >> 8}, I"; //set V0-vx to I
-                            case 0x65: return $"STO I, V0-V{(instr & 0x0F00) >> 8}"; //set V0-vx from I
+                        switch (nn) {
+                            case 7: return $"LDT V{x}"; //set vx to delay timer
+                            case 0xA: return $"WKY V{x}"; //wait for key and place it to vx (blocking)
+                            case 0x15: return $"SDT V{x}"; //set delay timer to vx
+                            case 0x18: return $"SST V{x}"; //set sound timer to vx
+                            case 0x1E: return $"ADD IR, V{x}"; //add vx to ir
+                            case 0x29: return $"LSP V{x}"; //load sprite to i from vx
+                            case 0x33: return $"BCD V{x}"; //store bcd of vx in i
+                            case 0x55: return $"STO V0-V{x}, I"; //set V0-vx to I
+                            case 0x65: return $"STO I, V0-V{x}"; //set V0-vx from I
                         }
                         return $"SYS ${instr & 0x0FFF:X4}";
                     default: return $"RAW ${instr:X4}";
